2f02fc191bfb0736916f81c14cd9a0bee7dcce82
Patrik Berglund <aptrik@users.noreply.github.com>
2013-11-08 14:05:02 +0100

    Added Guardfile to ruby-mode.

--- mode-mappings.el
+++ mode-mappings.el
@@ -19,6 +19,7 @@
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
