4d29235f36b87b3fe43c7c547165c0ae7fb3f564
Patrik Berglund <aptrik@users.noreply.github.com>
2013-11-09 11:11:56 +0100

    Added function 'buffer-file-name-body.

--- defuns.el
+++ defuns.el
@@ -20,6 +20,12 @@
   (list 'if (display-graphic-p)
         (cons 'progn x)))
 
+(defun buffer-file-name-body ()
+  "Buffer file name stripped of directory and extension."
+  (if (buffer-file-name)
+      (file-name-nondirectory (file-name-sans-extension (buffer-file-name)))
+    (cadr (reverse (split-string (dired-current-directory) "/")))))
+
 (defun comment-or-uncomment-current-line-or-region ()
   "Comments or uncomments current current line or whole lines in region."
   (interactive)
