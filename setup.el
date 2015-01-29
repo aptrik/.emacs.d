d901bdbf16e9b2200903feb2eaed5219b4feaca2
Patrik Berglund <aptrik@users.noreply.github.com>
2015-01-29 14:18:05 +0100

    Customized ibuffer.

--- setup.el
+++ setup.el
@@ -495,15 +495,38 @@
   :commands (hl-tags-mode))
 
 
+(use-package ibuffer
+  :bind (([remap list-buffers] . ibuffer))
+  :config (setq ibuffer-formats
+                '((mark modified read-only vc-status-mini " "
+                        (name 18 18 :left :elide)
+                        " "
+                        (size 9 -1 :right)
+                        " "
+                        (mode 16 16 :left :elide)
+                        " "
+                        (vc-status 16 16 :left)
+                        " "
+                        filename-and-process)
+                  (mark modified read-only " "
+                        (name 18 18 :left :elide)
+                        " "
+                        (size 9 -1 :right)
+                        " "
+                        (mode 16 16 :left :elide)
+                        " " filename-and-process)
+                  (mark " "
+                        (name 16 -1)
+                        " " filename))))
+
+
 (use-package ibuffer-vc
-  :init
-  (progn
-    (add-hook 'ibuffer-hook
-              (lambda ()
-                (ibuffer-vc-set-filter-groups-by-vc-root)
-                (unless (eq ibuffer-sorting-mode 'alphabetic)
-                  (ibuffer-do-sort-by-alphabetic)))))
-  :bind (("C-x C-b" . ibuffer)))
+  :defer t
+  :init (add-hook 'ibuffer-hook
+                  (lambda ()
+                    (ibuffer-vc-set-filter-groups-by-vc-root)
+                    (unless (eq ibuffer-sorting-mode 'alphabetic)
+                      (ibuffer-do-sort-by-alphabetic)))))
 
 
 (use-package iedit
