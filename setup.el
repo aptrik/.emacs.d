38c85fb32d68ffa9918df4c686bc53a097b7c721
Patrik Berglund <aptrik@users.noreply.github.com>
2016-01-08 00:43:20 +0100

    New comint config.

--- setup.el
+++ setup.el
@@ -281,6 +281,19 @@
   :defer t)
 
 
+(use-package comint
+  :defer t
+  :config
+  (progn
+    (define-key comint-mode-map (kbd "<down>") #'comint-next-input)
+    (define-key comint-mode-map (kbd "<up>") #'comint-previous-input)
+    (define-key comint-mode-map (kbd "C-n") #'comint-next-input)
+    (define-key comint-mode-map (kbd "C-p") #'comint-previous-input)
+    (define-key comint-mode-map (kbd "C-r") #'comint-history-isearch-backward)
+    (setf comint-prompt-read-only t
+          comint-history-isearch t)))
+
+
 (use-package company
   :defer 5
   :config
