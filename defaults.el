48a7d126a67b2a3952076250631084640c5aa00a
Patrik Berglund <aptrik@users.noreply.github.com>
2016-05-12 20:54:09 +0200

    Increased the gc threshold.

--- defaults.el
+++ defaults.el
@@ -52,6 +52,7 @@
  eval-expression-print-level     nil
  frame-resize-pixelwise          t
  garbage-collection-messages     nil
+ gc-cons-threshold               20000000
  history-delete-duplicates       t
  history-length                  t
  kill-ring-max                   200
