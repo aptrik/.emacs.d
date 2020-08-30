75275ccf5075109c991fec9dab5f4e5d2cd61889
Patrik Berglund <aptrik@users.noreply.github.com>
2020-08-30 17:24:12 +0200

    Fix indentation

--- setup.el
+++ setup.el
@@ -590,13 +590,13 @@
   (flycheck-add-next-checker 'python-flake8 '(t . python-pylint))
 
   (flycheck-define-checker proselint
-                           "A linter for prose."
-                           :command ("proselint" source-inplace)
-                           :error-patterns
-                           ((warning line-start (file-name) ":" line ":" column ": "
-                                     (id (one-or-more (not (any " "))))
-                                     (message) line-end))
-                           :modes (text-mode markdown-mode gfm-mode))
+    "A linter for prose."
+    :command ("proselint" source-inplace)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+              (id (one-or-more (not (any " "))))
+              (message) line-end))
+    :modes (text-mode markdown-mode gfm-mode))
   (add-to-list 'flycheck-checkers 'proselint)
 
   (use-package flycheck-color-mode-line
