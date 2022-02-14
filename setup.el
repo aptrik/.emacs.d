c5cddfa49ac3124a287e4fad4083910a83e4fb38
Patrik Berglund <aptrik@users.noreply.github.com>
2022-02-14 21:29:33 +0100

    Uninstall dumb-jump and helm-gtags

--- setup.el
+++ setup.el
@@ -405,18 +405,6 @@
   (setq dtrt-indent-verbosity 1))
 
 
-(use-package dumb-jump
-  :defer t
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g b" . dumb-jump-back)
-         ("M-g q" . dumb-jump-quick-look)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
-  :config
-  (setq dumb-jump-selector 'helm))
-
-
 (use-package ediff
   :defer t
   :commands (ediff-files ediff-buffers)
