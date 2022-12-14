141e2f92d2f3b400cae67f24021ef41b07cae63f
Patrik Berglund <aptrik@users.noreply.github.com>
2022-12-14 19:49:18 +0100

    Remove unused package mouse

--- key-bindings.el
+++ key-bindings.el
@@ -1,14 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-(unless window-system
-  (use-package mouse
-    :ensure nil
-    :defer 3
-    :init
-    (context-menu-mode 1))
-  (xterm-mouse-mode t))
-
-
 ;;(global-unset-key (kbd "C-x C-c"))
 ;;(global-set-key (kbd "C-x C-c") 'delete-frame)
 ;;(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
