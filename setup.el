0cab83e3ed91db98b3234fe73e89e2bc1842daa7
Patrik Berglund <aptrik@users.noreply.github.com>
2023-08-03 13:53:13 +0200

    Install all-the-icons-completion

--- setup.el
+++ setup.el
@@ -18,6 +18,13 @@
   :hook (ahg-status-mode . turn-on-truncate-lines))
 
 
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
+
 (use-package ansible-doc
   :hook (yaml-mode . ansible-doc-mode))
 
