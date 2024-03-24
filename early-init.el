60a86a29085d3228e6fa5099dc8e26ead62a6a33
Patrik Berglund <aptrik@users.noreply.github.com>
2024-03-24 13:45:23 +0100

    Use LSP_USE_PLISTS

--- early-init.el
+++ early-init.el
@@ -7,5 +7,8 @@
           (lambda ()
             (setq gc-cons-threshold (* 2 1000 1000))))
 
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+(setenv "LSP_USE_PLISTS" "true")
+
 (provide 'early-init)
 ;;; early-init.el ends here
